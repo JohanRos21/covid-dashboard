2b3c436592e6f312add53ea010d244ed8bfeca63
2002arnethe <93696533+2002arnethe@users.noreply.github.com>
2025-08-21 18:09:22 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from io import StringIO
+import matplotlib.pyplot as plt
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 
 st.set_page_config(page_title="COVID-19 Viz – Pregunta 2", layout="wide")
 
